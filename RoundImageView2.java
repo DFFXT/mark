0f1b561c44a8dd48e69a35f0922fa07555c69a26
qinfei <qinfei@pwrd.com>
2019-10-08 10:28:53 +0800

    2019/10/08 周二-10:28:53.70

--- RoundImageView2.java
+++ RoundImageView2.java
@@ -30,35 +30,25 @@ public class RoundImageView2 extends AppCompatImageView {
     /**
      * 圆角ImageView，支持padding scaleType
      * saveLayer会新建一个图层,后续操作会在新图层上进行
+     *
      * @param canvas canvas
      */
     @Override
     protected void onDraw(Canvas canvas) {
         Drawable drawable = getDrawable();
         if (drawable == null) return;
-        int width;
-        int height;
-        if (getCropToPadding()) {
-            int left = getScrollX() + getPaddingStart();
-            int top = getScrollY() + getPaddingTop();
-            int right = getScrollX() + getRight() - getLeft() - getPaddingEnd();
-            int bottom = getScrollY() + getBottom() - getTop() - getPaddingBottom();
-            canvas.clipRect(left, top, right, bottom);
-            width = right - left;
-            height = bottom - top;
-        } else {
-            width = getWidth() - getPaddingEnd() - getPaddingStart();
-            height = getHeight() - getPaddingBottom() - getPaddingTop();
-            canvas.clipRect(getPaddingStart(), getPaddingTop(), getWidth() - getPaddingEnd(), getHeight() - getPaddingBottom());
-        }
+        int width = getWidth() - getPaddingEnd() - getPaddingStart();
+        int height = getHeight() - getPaddingBottom() - getPaddingTop();
+        canvas.clipRect(getPaddingStart(), getPaddingTop(), getWidth() - getPaddingEnd(), getHeight() - getPaddingBottom());
         canvas.translate(getPaddingStart(), getPaddingTop());
-        canvas.saveLayer(0, 0, width, height, mPaint);//园图层
+
+        int save = canvas.saveLayer(0, 0, width, height, mPaint);//园图层
         canvas.drawRoundRect(0, 0, width, height, width / 2f, height / 2f, mPaint);
         mPaint.setXfermode(mode);
         canvas.saveLayer(0, 0, width, height, mPaint);//图片图层，设置paint mode
         canvas.concat(getImageMatrix());
         drawable.draw(canvas);
-        canvas.restore();//根据paint规则图层重叠
+        canvas.restoreToCount(save);//根据paint规则图层重叠
         mPaint.setXfermode(null);
     }
 }
